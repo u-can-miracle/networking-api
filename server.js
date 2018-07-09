c772967a3c0792f3bd1e55cc2cf60e8f0dd70d42
Nikolay Podolnyy <cloudsmoonlight@gmail.com>
2018-07-09 23:33:32 +0300

    test

--- server.js
+++ server.js
@@ -2,30 +2,30 @@ import 'babel-polyfill'
 import express from 'express'
 
 import envConfig from './server/config/config'
-import appConfig from './server/config/express'
-import errorHandling from './server/config/errorHandling'
-import authConfig from './server/config/passport'
-import rootRouter from './server/routers'
-
-// setup connection and associations
-import './server/db'
+// import appConfig from './server/config/express'
+// import errorHandling from './server/config/errorHandling'
+// import authConfig from './server/config/passport'
+// import rootRouter from './server/routers'
+//
+// // setup connection and associations
+// import './server/db'
 
 const app = express()
 
-
-appConfig(app)
-
-authConfig(app)
-
-rootRouter(app)
-
-errorHandling(app)
-
-
-process.on('uncaughtException', (err) => {
-  console.log('Oops! err: ', err) //TODO change to winston
-})
-
+//
+// appConfig(app)
+//
+// authConfig(app)
+//
+// rootRouter(app)
+//
+// errorHandling(app)
+//
+//
+// process.on('uncaughtException', (err) => {
+//   console.log('Oops! err: ', err) //TODO change to winston
+// })
+//
 
 
 app.listen(envConfig.port, function(){
