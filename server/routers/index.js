6a6e5d9dc68816b2ae5b58a700aa127cc352a648
Nikolay Podolnyy <cloudsmoonlight@gmail.com>
2018-08-02 23:29:30 +0300

    after elastic test

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,22 +2,9 @@ import api from './api'
 import OAuthLogin from './OAuthLogin'
 import serverRender from './serverRender'
 
-import config from '../config/config'
-import { postJsonToUrl } from '../helpers'
-const { elastic: { url } } = config
-
-
 export default function rootRouter(app){
   app.use(api)
   app.use(OAuthLogin)
-
-	app.post('/ping', async (req, res) => {
-		const elsUrl = `${url}/test`
-		console.log('elsUrl', elsUrl)
-		const result = await postJsonToUrl(elsUrl, {})
-
-		res.send(result)
-	})
 
   app.get('*', serverRender)
 }
