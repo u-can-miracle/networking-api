34248704ba641a0d1476327f558203739bdc0f60
Nikolay Podolnyy <cloudsmoonlight@gmail.com>
2018-08-14 09:13:17 +0300

    change db config referance

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
 module.exports = {
 	'development': {
-		'username': 'postgres',
-		'password': 'postgres',
-		'database': 'network_dev',
-		'host': '127.0.0.1',
-		'port': '5432',
+		'username': process.env.DB_USER,
+		'password': process.env.DB_PASS,
+		'database': process.env.INITIAL_DB,
+		'host': process.env.DB_HOST,
+		'port': process.env.DB_PORT,
 		'dialect': 'postgres'
 	},
 	'test': {
-		'username': 'postgres',
-		'password': 'postgres',
-		'database': 'network_test',
-		'host': '127.0.0.1',
-		'port': '5432',
+		'username': process.env.DB_USER,
+		'password': process.env.DB_PASS,
+		'database': process.env.INITIAL_DB,
+		'host': process.env.DB_HOST,
+		'port': process.env.DB_PORT,
 		'dialect': 'postgres'
 	},
 	'production': {
