eb8b7c92f264b64087f4cf7c513e3e3df14bdcb4
Nikolay Podolnyy <cloudsmoonlight@gmail.com>
2018-08-21 08:19:08 +0300

    log

--- server/routers/index.js
+++ server/routers/index.js
@@ -3,6 +3,11 @@ import OAuthLogin from './OAuthLogin'
 import serverRender from './serverRender'
 
 export default function rootRouter(app){
+	app.use((req, res, next) => {
+		console.log('req.url', req.url)
+		next()
+	})
+
   app.use(api)
   app.use(OAuthLogin)
 
